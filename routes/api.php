78b47fa9ffcbce9da25118f8950b8c4305e92c75
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-15 17:49:04 -0300

    feat: criado rotas protegidas pelo sanctum

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
+    Route::get('/hello', function () {
+        return response()->json(['hello' => 'world']);
+    });
+});
