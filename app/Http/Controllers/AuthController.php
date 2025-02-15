619178a84ac6e79ae3944f5867301870ca337543
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-15 17:45:36 -0300

    feat: criado action para registro de usuário

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|max:255'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $user = User::fromUSer($user);
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso!',
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+}
