5c02d3e4f67132107ee77ab24eb296e829d6507b
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-15 18:42:03 -0300

    feat: criado rota de logout

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
