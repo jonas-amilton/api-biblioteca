d50814d6871150027794c3ab6c7f9b3e31749138
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-16 21:29:50 -0300

    refactor: alterado Controller da action me

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
-    Route::get('/me', [AuthController::class, 'me']);
+    Route::get('/me', [UserController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
