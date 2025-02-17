c81899597e98ed36c041ad56bde20dd800fd22fe
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-16 21:24:56 -0300

    refactor: alterado para proteger rota de logout

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
