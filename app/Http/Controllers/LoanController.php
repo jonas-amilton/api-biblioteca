b99d378b0d538b199812c0f609db62b7465d0162
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-19 22:32:57 -0300

    delete: removido extensao do Controller sample do laravel

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -6,7 +6,7 @@ use App\Models\Loan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class LoanController extends Controller
+class LoanController
 {
     public function store(Request $request)
     {
