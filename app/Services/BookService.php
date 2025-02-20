a17951a9238396d4b54aa82ccbc124e6505fe0ec
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-19 22:41:47 -0300

    feat: criado método para listar livros disponíveis

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -8,5 +8,17 @@ use App\Models\{
 
 class BookService
 {
-//
+    public function booksAvailable($idNotAvailable)
+    {
+        return Book::select(
+            'books.id',
+            'books.title',
+            'books.isbn',
+            'books.author',
+            'books.publisher',
+            'books.subject',
+            'books.summary'
+        )->whereNotIn('books.id', $idNotAvailable)
+            ->get();
+    }
 }
