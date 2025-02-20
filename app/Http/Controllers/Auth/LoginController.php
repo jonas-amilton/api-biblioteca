babe2d6ef8bff9ffdbb5a5d7618ffb1c32d99c80
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-20 20:41:59 -0300

    feat: criado controller para login de usuário, e adicionado a rota api

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{
+    Validator,
+    Hash
+};
+
+class LoginController
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+            ], 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Credenciais inválidas'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\{
+    RegisterController,
+    LoginController
+};
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Book\{
     AvailableBookController,
@@ -16,7 +19,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterController::class);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
