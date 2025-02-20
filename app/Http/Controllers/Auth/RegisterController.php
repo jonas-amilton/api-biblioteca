c60bfe1a42835c2ed732ec09f16c9f2d8a942512
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-20 20:38:33 -0300

    feat: criado controller para registrar novo usuário, e adicionado a rota api

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{
+    Hash,
+    Validator
+};
+
+class RegisterController
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+            ], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $user = User::fromUser($user);
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso!',
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Book\{
     AvailableBookController,
@@ -9,12 +10,12 @@ use App\Http\Controllers\Book\{
     UpdateBookController,
     BookByIdController
 };
-use App\Http\Controllers\BookController;
+
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', RegisterController::class);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
