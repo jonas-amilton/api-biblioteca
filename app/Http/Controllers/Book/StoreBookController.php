bdf851da55c8c9dabd6b9d86f0bfe246c4d5e8df
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-20 22:09:19 -0300

    refactor: alterado para utilizar request e validar campos da requisição

--- app/Http/Controllers/Book/StoreBookController.php
+++ app/Http/Controllers/Book/StoreBookController.php
@@ -2,39 +2,14 @@
 
 namespace App\Http\Controllers\Book;
 
+use App\Http\Requests\StoreBookRequest;
 use App\Models\Book;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class StoreBookController
 {
-    public function __invoke(Request $request)
+    public function __invoke(StoreBookRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'isbn' => 'required|string|max:13|unique:books',
-            'title' => 'required|string|max:60',
-            'author' => 'required|string|max:60',
-            'publisher' => 'required|string|max:255',
-            'publication_date' => 'required|date',
-            'subject' => 'required|string|max:50',
-            'summary' => 'required|string|max:255'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'error' => $validator->errors()->first(),
-            ], 400);
-        }
-
-        $book = Book::create([
-            'isbn' => $request->isbn,
-            'title' => $request->title,
-            'author' => $request->author,
-            'publisher' => $request->publisher,
-            'publication_date' => $request->publication_date,
-            'subject' => $request->subject,
-            'summary' => $request->summary
-        ]);
+        $book = Book::create($request->validated());
 
         if (!$book) {
             return response()->json([
