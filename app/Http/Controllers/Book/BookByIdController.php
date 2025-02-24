17dff9e325c30aff90bf29b082d99041db1eb57c
jonas-amilton <jonas96@edu.unisinos.br>
2025-02-23 21:37:27 -0300

    feat: implementado novo service para separar regras de negócio do controller

--- app/Http/Controllers/Book/BookByIdController.php
+++ app/Http/Controllers/Book/BookByIdController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers\Book;
 
 use App\Models\Book;
+use App\Services\BookService;
 use Illuminate\Http\Request;
 
 class BookByIdController
 {
+    private $bookService;
+
+    public function __construct(BookService $bookService){
+        $this->bookService = $bookService;
+    }
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        $book = Book::find($request->id);
+        $book = $this->bookService->findBookById($request->id);
 
         if (!$book) {
             return response()->json([
